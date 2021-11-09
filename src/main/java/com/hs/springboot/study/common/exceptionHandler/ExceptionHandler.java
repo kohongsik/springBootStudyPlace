6ea0f761379489308b83398798b621d3f8811df6
hongsik <gohongsig@kongsigi.local>
2021-11-09 22:01:22 +0900

    custom exception handler 추가

--- src/main/java/com/hs/springboot/study/common/exceptionHandler/ExceptionHandler.java
+++ src/main/java/com/hs/springboot/study/common/exceptionHandler/ExceptionHandler.java
@@ -10,7 +10,16 @@ import javax.servlet.http.HttpServletRequest;
 @ControllerAdvice
 public class ExceptionHandler {
     private Logger logger = LoggerFactory.getLogger("board");
-
+    @org.springframework.web.bind.annotation.ExceptionHandler(UnauthorizedException.class)
+    public ModelAndView unauthorizedExceptionHandler(HttpServletRequest request, UnauthorizedException exception) {
+        ModelAndView mv = new ModelAndView("/error/error_default");
+        mv.addObject("exception", exception);
+        // error log table < insert
+        // error
+        logger.debug("unauthorizedExceptionHandler", exception);
+        logger.error("exception", exception);
+        return mv;
+    }
     @org.springframework.web.bind.annotation.ExceptionHandler(Exception.class)
     public ModelAndView defaultExceptionHandler(HttpServletRequest request, Exception exception) {
         ModelAndView mv = new ModelAndView("/error/error_default");
